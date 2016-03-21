1131e0d0946c60cb1154107aed3e132ebeb100e6
EpicSteve33 <epicsteve33@gmail.com>
2016-03-21 16:59:12 -0700

    Update SurvivalGamesV3.php

--- src/SurvivalGamesV3/SurvivalGamesV3.php
+++ src/SurvivalGamesV3/SurvivalGamesV3.php
@@ -407,9 +407,9 @@ class GameSender extends PluginTask {
 							{
 								$timeToStart--;
 								foreach($playersArena as $pl)
+					                        {
+									$pl->sendPopup(TextFormat::GRAY . "Starting in " . $timeToStart . " Seconds");
 								{
-									$pl->sendTip(TextFormat::GOLD . $timeToStart . " seconds");
-								}
 								if($timeToStart<=0)
 								{
 									$this->noDamageTicks = 300;
